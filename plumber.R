b52477106c5d21703c67a69e1c23986f7742c731
Kateryna <45816115+kateryna-git@users.noreply.github.com>
2020-10-13 16:34:04 +0300

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -11,19 +11,6 @@ library(timetk)
 raw_data <- read_csv("sales_data_sample.csv") 
 
 
-#* Plot a time series plot of the data
-#* @serializer htmlwidget
-#* @get /plot
-function(){
- raw_data %>%
-    select(SALES, ORDERDATE) %>% 
-    mutate(date = mdy_hm(ORDERDATE) %>% as_datetime()) %>%
-    group_by(date) %>%
-    summarise(value = sum(SALES)) %>%
-    ungroup() %>%
-    plot_time_series(date, value, .interactive = TRUE)
-
-}
 
 
 
